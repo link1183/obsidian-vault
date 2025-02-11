0a96fc9ab0f4826be62bdeb1151677e611c367b6
link1183 <adrien.gunther@gmail.com>
2025-02-11 14:19:09 +0100

    [laptop] vault backup: 2025-02-11 14:19:09

--- RE/code/x86/RE-12.c
+++ RE/code/x86/RE-12.c
@@ -1,1 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void unreachableFunction(void) {
+  printf("I'm hacked! I am a hidden function!\n");
+  exit(0);
+}
+
+int main(void) {
+  printf("Hello World!\n");
+
+  return 0;
+}
