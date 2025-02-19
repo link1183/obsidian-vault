399ce41f69de979978caa43691f9bdda2bc5e7dd
link1183 <adrien.gunther@gmail.com>
2025-02-19 07:11:55 +0100

    [laptop] vault backup: 2025-02-19 07:11:55

---
+++ RE/code/x86/22/moving_immediate_data.asm
@@ -0,0 +1,21 @@
+;moving_immediate_data: mov immediate data between registers & memory
+
+section .data
+
+section .bss
+  buffer resb 1
+
+section .text
+  global _start
+
+_start:
+  nop
+
+mov_immediate_data_to_register:
+  mov eax, 100 ;mov 100 into eax
+  mov byte[buffer], 0x50 ;mov 0x50 into the buffer memory location
+
+exit:
+  mov eax, 1
+  mov ebx, 1
+  int 0x80
--- RE/code/x86/22/moving_immediate_data.s
+++ RE/code/x86/22/moving_immediate_data.s
@@ -15,3 +15,8 @@ _start:
 mov_immediate_data_to_register:
   movl $100, %eax
   movl $0x50, buffer
+
+exit:
+  movl $1, %eax
+  movl $0, %ebx
+  int $0x80
